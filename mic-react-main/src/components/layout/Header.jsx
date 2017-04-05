6d5001718499b664118d1c79b73692e77b0c3f77
Baron Paredes <baronpatrick.paredes@gmail.com>
2017-04-06 00:44:04 +0800

    CSS tweaks

--- mic-react-main/src/components/layout/Header.jsx
+++ mic-react-main/src/components/layout/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Grid, Row, Col, Image } from 'react-bootstrap/lib'
+import HeaderBrand from './HeaderBrand'
 
 export default class Header extends React.Component {
     render() {
@@ -10,23 +11,13 @@ export default class Header extends React.Component {
                         <Image src="/img/mic-logo-1.png" className="brand-logo" />
                     </Col>
                     <Col lg={6} md={6} smHidden xsHidden className="header-col">
-                        <div className="brand-info">
-                            <h1 className="brand-title">
-                                <strong>M</strong>edical and <strong>I</strong>nsurance <strong>C</strong>are
-                            </h1>
-                            <h3>Your Trusted Service Partner</h3>
-                        </div>
+                        <HeaderBrand />
                     </Col>
                     <Col lg={3} md={3} sm={6} xs={6} className="header-col">
                         <Image src="/img/mic-logo-2.png" className="brand-logo" />
                     </Col>
                     <Col lgHidden mdHidden sm={12} xs={12}>
-                        <div className="brand-info">
-                            <h1 className="brand-title">
-                                <strong>M</strong>edical and <strong>I</strong>nsurance <strong>C</strong>are
-                            </h1>
-                            <h3>Your Trusted Service Partner</h3>
-                        </div>
+                        <HeaderBrand />
                     </Col>
                 </Row>
             </Grid>
---
+++ mic-react-main/src/components/layout/HeaderBrand.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HeaderBrand = () => {
+    return (
+        <div className="brand-info">
+            <h1 className="brand-title">
+                <span className="brand-m">M</span>edical and <span className="brand-i">I</span>nsurance <span className="brand-c">C</span>are
+            </h1>
+            <h3>Your Trusted Service Partner</h3>
+        </div>
+    );
+};
+
+export default HeaderBrand;

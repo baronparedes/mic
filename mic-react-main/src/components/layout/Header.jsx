9c0f1e0ac4ee8fec13f0c7d7f7749d48d9a02803
Baron Paredes <baronpatrick.paredes@gmail.com>
2017-04-06 00:37:43 +0800

    Tweaked header for mobile and tablet devices

--- mic-react-main/src/components/layout/Header.jsx
+++ mic-react-main/src/components/layout/Header.jsx
@@ -6,10 +6,10 @@ export default class Header extends React.Component {
         return (
             <Grid fluid>
                 <Row>
-                    <Col md={3} sm={12} className="header-col">
+                    <Col lg={3} md={3} sm={6} xs={6} className="header-col">
                         <Image src="/img/mic-logo-1.png" className="brand-logo" />
                     </Col>
-                    <Col md={6} sm={12} className="header-col">
+                    <Col lg={6} md={6} smHidden xsHidden className="header-col">
                         <div className="brand-info">
                             <h1 className="brand-title">
                                 <strong>M</strong>edical and <strong>I</strong>nsurance <strong>C</strong>are
@@ -17,8 +17,16 @@ export default class Header extends React.Component {
                             <h3>Your Trusted Service Partner</h3>
                         </div>
                     </Col>
-                    <Col md={3} sm={12} className="header-col">
+                    <Col lg={3} md={3} sm={6} xs={6} className="header-col">
                         <Image src="/img/mic-logo-2.png" className="brand-logo" />
+                    </Col>
+                    <Col lgHidden mdHidden sm={12} xs={12}>
+                        <div className="brand-info">
+                            <h1 className="brand-title">
+                                <strong>M</strong>edical and <strong>I</strong>nsurance <strong>C</strong>are
+                            </h1>
+                            <h3>Your Trusted Service Partner</h3>
+                        </div>
                     </Col>
                 </Row>
             </Grid>
